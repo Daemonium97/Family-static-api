8818e450898bf0bca5f0d86e229a9403bb88de4e
Daemonium97 <daemonium97@gmail.com>
2021-02-24 00:00:25 +0000

    Aloo

--- src/datastructures.py
+++ src/datastructures.py
@@ -6,21 +6,41 @@ update this file to implement the following already declared methods:
 - update_member: Should update a member from the self._members list
 - get_member: Should return a member from the self._members list
 """
-from random import randint
+from random import randint, request, json, jsonify
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+           { "id" : self._generateId(),
+            "first_name" : "John",
+            "last_name" : "Jackson",
+            "age" : 33,
+            "Lucky Numbers" : [7, 13, 22]},
+           { "id" : self._generateId(),
+            "first_name" : "Jane",
+            "last_name" : "Jackson",
+            "age" : 35,
+            "Lucky Numbers" : [10, 14, 3]},
+           { "id" : self._generateId(),
+            "first_name" : "Jimmy",
+            "last_name" : "Jackson",
+            "age" : 5,
+            "Lucky Numbers" : [1]}
+
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
-
+    @app.route('/members', methods=['POST'])
     def add_member(self, member):
         # fill this method and update the return
+        
+        self._members.append()
+        
         pass
 
     def delete_member(self, id):
